44b01f1caa7b9f1b9e89e638034554bcf2aac38f
Matthew-PC <matburren@gmail.com>
2020-03-05 14:36:31 +0000

    implemented insert and get functions for user table

--- classes/DBContext.php
+++ classes/DBContext.php
@@ -2,12 +2,14 @@
 
 class DBContext
 {
-	private $servername = "";
-	private $username = "";
+	private $servername = "localhost";
+	private $username = "root";
 	private $password = "";
 	private $dbname = "";
-	
-	private __construct()
+
+	private $connection;
+
+	private function __construct()
 	{
 		$this->connection=mysqli_connect($this->servername,$this->username,$this->password,$this->dbname);
 
@@ -15,6 +17,27 @@ class DBContext
             die("Database connection failed: " . $this->connection->connect_error);
         }
 	}
+
+	public function InsertUser($email, $firstname, $surname, $password)
+    {
+        $query = mysqli_prepare($this->connection, "CALL insert_User(?,?,?,?)");
+        mysqli_stmt_bind_param($query, "ssss", $email, $firstname, $surname, $password);
+        mysqli_stmt_execute($query);
+    }
+
+    public function GetUsers()
+    {
+        $query = mysqli_query($this->connection, "SELECT * FROM User")
+        or die (mysqli_error($this->connection));
+
+        $users = [];
+
+        //Rows need to be retrieved this was to preserve the column names
+        while ($row = mysqli_fetch_array($query))
+            array_push($users, $row);
+
+        return $users;
+    }//This function returns spurious data and can be improved
 }
 
 ?>
