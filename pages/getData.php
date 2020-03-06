3c96f310e3870efbe82e99ec8199f0c7d196677c
ItsMeOl1 <44836648+ItsMeOl1@users.noreply.github.com>
2020-03-06 09:26:00 +0000

    Update getData.php

--- pages/getData.php
+++ pages/getData.php
@@ -1,13 +1,5 @@
-$connection = mysql_connect("localhost", "root", "");
-$db = mysql_select_db("company", $connection);
+<?php
+include_once "header.php"
 
-$query = mysql_query("select * from tablename", $connection);
-
-<span>Name:</span> <?php echo $row1['employee_name']; ?>
-<span>E-mail:</span> <?php echo $row1['employee_email']; ?>
-<span>Contact No:</span> <?php echo $row1['employee_contact']; ?>
-<span>Address:</span> <?php echo $row1['employee_address']; ?>
-
-mysql_close($connection);
-
-<! todo connect, select, echos>
+$db->GetSleepData();
+?>
